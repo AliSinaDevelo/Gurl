64341d10e28883fae224ff271e3e6223d5f20d69
AliSina Karimi <100498538+AliSinaDevelo@users.noreply.github.com>
2024-07-29 21:58:31 +0200

    Gurl's base server return some data from its API endpoints

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("a good Gurl shortens URLs with ease !")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Ready to shorten URLs like a good Gurl !",
+		})
+	})
+	// fmt.Println("a good Gurl shortens URLs with ease !")\
+
+	err := r.Run(":8080")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start web server -\nThis renders Gurl sad :(\nError: %v", err))
+	}
 }
